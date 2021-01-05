eb6ec812946ce0a4f001000276d7267956611a7b
Devorah Posner <dleahposner@gmail.com>
2021-01-05 22:42:30 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -36,10 +36,13 @@ def turn(board)
 end
 
 def play(board)
-  counter = 0 
-  until counter == 9
+  # counter = 0 
+  # until counter == 9
+  #   turn(board)
+  #   counter += 1 
+  # end
+  9.times do 
     turn(board)
-    counter += 1 
   end
 end
 
